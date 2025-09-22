02453b4dc69179248668a445272135986d20def9
konglei <lei.kong@cellwise-semi.com>
2025-09-22 15:40:40 +0800

    a

--- User/main.c
+++ User/main.c
@@ -41,9 +41,9 @@ int main(void)
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
     SystemCoreClockUpdate();
     Delay_Init();
-    USART_Printf_Init(115200);
-    printf("SystemClk:%d\r\n", SystemCoreClock);
-    printf("ChipID:%08x\r\n", DBGMCU_GetCHIPID());
+    // USART_Printf_Init(115200);
+    // printf("SystemClk:%d\r\n", SystemCoreClock);
+    // printf("ChipID:%08x\r\n", DBGMCU_GetCHIPID());
 
     GPIO_InitTypeDef GPIO_InitStructure = { 0 };
 
