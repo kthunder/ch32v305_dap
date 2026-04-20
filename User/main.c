e9f29747001f60861a8db4463ffc147d3b402e2f
konglei <lei.kong@cellwise-semi.com>
2026-04-20 18:30:33 +0800

    a

--- User/main.c
+++ User/main.c
@@ -64,7 +64,7 @@ int main(void)
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOC, &GPIO_InitStructure);
 
